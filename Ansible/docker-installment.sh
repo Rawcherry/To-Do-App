ecf48df55e248289bbb4421b80816cc2ec7a19e0
Rawcherry <160492481+Rawcherry@users.noreply.github.com>
2025-07-04 09:58:34 +0000

    Update docker-installment.sh

--- Ansible/docker-installment.sh
+++ Ansible/docker-installment.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
+sudo dnf update -y
 
+sudo dnf remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine || true
+
+# Add Docker repository
+sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+
+# Install Docker Engine
+sudo dnf install -y docker-ce docker-ce-cli containerd.io
+
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Verify Docker
+docker --version
+
+sudo curl -L "https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+echo "Installation complete!"
